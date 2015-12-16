8e202a82948970e22119baab98e79bfd04370e6e
EpeTukone <fren.ilya@tut.by>
2015-12-16 23:05:31 +0300

    file

---
+++ task10.py
@@ -0,0 +1,7 @@
+def string_function(s):
+    spisok = s.split(' ')
+    return spisok
+
+if __name__ ==  "__main__":
+    s = "python is a best language "
+    print('spisok: {}'.format(string_function(s)))
