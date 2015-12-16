8c7c3c88ca5bbc52cc16d7f574e3ede21d8cc814
EpeTukone <fren.ilya@tut.by>
2015-12-17 01:43:47 +0300

    file

---
+++ task13.py
@@ -0,0 +1,8 @@
+a = tuple('123456789')
+
+def tuple_function(tuple_):
+    b = list(a)
+    new_tuple = tuple([i for i in b if int(i) % 2 == 0])
+    return new_tuple
+
+print("new tuple = {}".format(tuple_function(a)))
